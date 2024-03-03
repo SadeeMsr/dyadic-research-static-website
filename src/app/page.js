ff3956b4e73ed1a5005f64c9e8f2519547793af9
SadeeMsr <srrohan333@gmail.com>
2024-03-03 23:37:21 +0600

    Footer color change

--- src/app/page.js
+++ src/app/page.js
@@ -5,41 +5,43 @@ import OurServices from "@/components/OurServices";
 import OurWork from "@/components/OurWork";
 import TechCardsGrid from "@/components/TechCardsGrid";
 
-
 const carouselImg = [
   {
     id: 0,
-    path: "/assets/carousel-img-01.png"
+    path: "/assets/carousel-img-01.png",
   },
   {
     id: 1,
-    path: "/assets/carousel-img-02.png"
+    path: "/assets/carousel-img-02.png",
   },
   {
     id: 2,
-    path: "/assets/carousel-img-03.png"
-  }
-]
+    path: "/assets/carousel-img-03.png",
+  },
+];
 
-const placeholderTexts  = [
+const placeholderTexts = [
   {
     type: "heading2",
     text: "A simple solution is a smart one.",
   },
   {
     type: "heading2",
-    text: "Turns waste into resources.",
+    text: "Farmers as researchers, researchers as farmers.",
   },
   {
     type: "heading2",
-    text: "Farmers as researchers, researchers as farmers.",
+    text: "Turns waste into resources.",
   },
 ];
 
 export default function Home() {
   return (
     <>
-      <HeroBgCarousel carouselImg={carouselImg} placeholderTexts={placeholderTexts} />
+      <HeroBgCarousel
+        carouselImg={carouselImg}
+        placeholderTexts={placeholderTexts}
+      />
       <OurServices />
       <OurWork />
       <AboutUsTab />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bg-[#9bb6c4] text-white">
+    <div className="bg-[#b4c6e4] text-white">
       <div className="flex lg:flex-row flex-col pt-10 md:px-20 px-10 pb-20">
         <div className="lg:w-[57%] w-full flex flex-col mt-16">
           <div className="overflow-hidden lg:w-[70%] w-full rounded-lg opacity-80 hover:opacity-100 duration-500">
@@ -23,7 +23,11 @@ export default function Footer() {
               className="py-2 md:w-[30%] w-[60%] h-auto"
               alt="Random image"
             />
-            <p className="text-sm">In a nutshell, Dyadic Research Impacts is a modern research firm serving government institutions and non-governmental organizations.</p>
+            <p className="text-sm text-[#012D50]">
+              In a nutshell, Dyadic Research Impacts is a modern research firm
+              serving government institutions and non-governmental
+              organizations.
+            </p>
           </div>
           <div className="flex gap-5 cursor-pointer mt-10 text-slate-900">
             <Link target="_blank" href={"https://www.facebook.com/"}>
@@ -46,44 +50,52 @@ export default function Footer() {
         <div className="lg:w-[43%] w-full mt-16">
           <div className="gap-10 grid lg:grid-cols-2 grid-cols-1 text-sm">
             <div>
-              <h1 className="font-semibold">QUICK LINKS</h1>
-              <ul className="text-slate-300 cursor-pointer mt-2">
-                <li className="hover:text-slate-50 mt-2">Home</li>
-                <li className="hover:text-slate-50 mt-2">What We DO</li>
-                <li className="hover:text-slate-50 mt-2">Research & Publications</li>
-                <li className="hover:text-slate-50 mt-2">Contact us</li>
+              <h1 className="font-semibold text-[#012D50]">QUICK LINKS</h1>
+              <ul className="text-[#587792] cursor-pointer mt-2 ">
+                <li className="hover:text-[#40576b] mt-2">Home</li>
+                <li className="hover:text-[#40576b] mt-2">What We DO</li>
+                <li className="hover:text-[#40576b] mt-2">
+                  Research & Publications
+                </li>
+                <li className="hover:text-[#40576b] mt-2">Contact us</li>
               </ul>
             </div>
 
             <div>
-              <p className="uppercase font-semibold">Rubaga Division, Kampala, Uganda</p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
-              P.O Box 27388, Rubaga Road, Sembule B Zone
+              <p className="uppercase font-semibold text-[#012D50]">
+                Rubaga Division, Kampala, Uganda
               </p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
-              +256773928080, +256700595967
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
+                P.O Box 27388, Rubaga Road, Sembule B Zone
+              </p>
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
+                +256773928080, +256700595967
               </p>
             </div>
             <div>
-              <p className="uppercase font-semibold">Vatara, Dhaka-1230, Bangladesh</p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
-                Plot#1292, Road#60a, Block-L, Bashundhara R/A 
+              <p className="uppercase font-semibold text-[#012D50]">
+                Vatara, Dhaka-1230, Bangladesh
               </p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
+                Plot#1292, Road#60a, Block-L, Bashundhara R/A
+              </p>
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
                 +88017556222, +880192222011
               </p>
             </div>
 
             <div>
-              <p className="uppercase font-semibold"> Nairobi, Kenya</p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
-              Duduville Campus, Kasarani,
+              <p className="uppercase font-semibold text-[#012D50]">
+                {" "}
+                Nairobi, Kenya
               </p>
-              <p className="text-slate-300 hover:text-slate-50 cursor-pointer mt-2">
-              +256773928080, +256700595967
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
+                Duduville Campus, Kasarani,
+              </p>
+              <p className="text-[#587792] hover:text-[#40576b] cursor-pointer mt-2">
+                +256773928080, +256700595967
               </p>
             </div>
-
           </div>
         </div>
       </div>
